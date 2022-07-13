456e431bdcd056945436541957212557d17194f7
Scott Novak <snovak646@gmail.com>
2022-07-13 19:42:27 -0400

    Initial Commit

---
+++ PracticeC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PracticeC++
+//
+//  Created by Scott Novak on 7/13/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
